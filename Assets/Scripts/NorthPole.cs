2643811794a6141b12d6be8e00de0f363419434e
Habenaria <77216284+Albidus1@users.noreply.github.com>
2025-04-06 00:55:21 +0900

    250406
    자력 블록 / 플레이어 테스트

---
+++ Assets/Scripts/NorthPole.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class NorthPole : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = Color.red; // 🔴 N극 = 빨강
+    }
+}
---
+++ Assets/Scripts/SouthPole.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SouthPole : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.color = Color.blue; // 🔵 S극 = 파랑
+    }
+}
