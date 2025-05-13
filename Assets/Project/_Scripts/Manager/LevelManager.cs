f0c35982e15a34cf20da06f6d9ffcaa996745296
Habenaria08_notebook <ahtp7788@naver.com>
2025-05-13 21:47:44 +0900

    250513
    복구 테스트

--- Assets/Project/_Scripts/Manager/LevelManager.cs
+++ Assets/Project/_Scripts/Manager/LevelManager.cs
@@ -10,8 +10,13 @@ public class LevelManager : MySingleton<LevelManager>
 {
     public PlayerMovement playerPrefabs;
 
+    [Header("체크 포인트")]
+    [Tooltip("캐릭터를 강제로 이곳에서 스폰하고 싶을 때 사용할 체크포인트")]
     public CheckPoint debugSpawn;
-    public CheckPoint currentCheckPoint;
+
+    [Tooltip("현재 체크포인트")]
+    [MyReadOnly]public CheckPoint currentCheckPoint;
+
     public virtual List<CheckPoint> checkPoints { get; private set; }
 
 
