6a408017695c350e3527449817571b0036bab239
Habenaria08_notebook <ahtp7788@naver.com>
2025-10-13 17:01:04 +0900

    251013

--- Assets/Project/_Scripts/UI/Health/HealthCellUI.cs
+++ Assets/Project/_Scripts/UI/Health/HealthCellUI.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,16 +7,15 @@ using UnityEngine.UI;
 public class HealthCellUI : MonoBehaviour
 {
     public Transform healthContainer;
+    public RectTransform fullHPImage;
     public Sprite healthSprite;
-    public Sprite emptyHealthSprite;
 
     public int initializeHealthCells = 20;
     public float cellSize = 40f;
     public float offset = 5f;
 
     private bool m_initialized = false;
-    private List<GameObject> m_emptyHealthCells = new List<GameObject>();
-    private List<GameObject> m_healthCells = new List<GameObject>();
+    private List<RectTransform> m_healthCells = new List<RectTransform>();
 
 
 
@@ -39,43 +37,25 @@ public class HealthCellUI : MonoBehaviour
             }
         }
         
-        Vector2 size = emptyHealthSprite.bounds.size;
-        float ratio = size.x / size.y;
+        Vector2 size = fullHPImage.sizeDelta;
+        float cellSizeX = size.x / initializeHealthCells;
 
         for (int i = 0; i < initializeHealthCells; i++)
         {
-            GameObject emptyCell = new GameObject("EmptyCell_" + (i + 1));
-            emptyCell.transform.SetParent(healthContainer);
-            
-            RectTransform rt = emptyCell.AddComponent<RectTransform>();
-            rt.sizeDelta = new Vector2(cellSize * ratio, cellSize);    
-            rt.localScale = Vector3.one;
-            rt.localPosition = Vector3.zero;
-            rt.anchorMin = new Vector2(0f, 0.5f);
-            rt.anchorMax = new Vector2(0f, 0.5f);
-            rt.pivot = new Vector2(0f, 0.5f);
-            rt.anchoredPosition = new Vector2((cellSize + offset) * i, 0);
-            Image img = emptyCell.AddComponent<Image>();
-            img.sprite = emptyHealthSprite;
-
-            m_emptyHealthCells.Add(emptyCell);
-
-
             GameObject cell = new GameObject("Cell_" + (i + 1));
-            cell.transform.SetParent(emptyCell.transform);
-            rt = cell.AddComponent<RectTransform>();
-            rt.sizeDelta = new Vector2(cellSize * ratio, cellSize);
+            RectTransform rt = cell.AddComponent<RectTransform>();
+            cell.transform.SetParent(healthContainer.transform);
+            rt.sizeDelta = new Vector2(size.x, size.y);
             rt.anchorMin = new Vector2(0f, 0.5f);
             rt.anchorMax = new Vector2(0f, 0.5f);
             rt.pivot = new Vector2(0f, 0.5f);
             rt.localScale = Vector3.one;
             rt.localPosition = Vector3.zero;
-            img = cell.AddComponent<Image>();
+            Image img = cell.AddComponent<Image>();
             img.sprite = healthSprite;
 
-            m_healthCells.Add(cell);
-
-            emptyCell.SetActive(false);
+            m_healthCells.Add(rt);
+            cell.SetActive(false);
         }
 
         m_initialized = true;
@@ -83,17 +63,23 @@ public class HealthCellUI : MonoBehaviour
 
     private void SetCell(int _currentHealth, int _maxHealth)
     {
+        Vector2 size = fullHPImage.sizeDelta;
+        float cellSizeX = size.x / _maxHealth;
+        size.x = cellSizeX;
+
         for (int i = 0; i < _maxHealth; i++)
         {
-            m_emptyHealthCells[i].SetActive(true);
-
             if (i < _currentHealth)
             {
-                m_healthCells[i].SetActive(true);
+                m_healthCells[i].sizeDelta = size;
+
+                Vector2 position = new Vector2(cellSizeX * i, 0);
+                m_healthCells[i].localPosition = position;
+                m_healthCells[i].gameObject.SetActive(true);
             }
             else
             {
-                m_healthCells[i].SetActive(false);
+                m_healthCells[i].gameObject.SetActive(false);
             }
         }
     }
